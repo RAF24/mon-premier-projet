57f28ff7bc520f1d68d561aa48abc2a710428a8c
Raf <rafiou.diallo24@gmail.com>
2019-08-17 14:19:33 +0200

    création du composant editAppareil pour ajouter une nouvel appreil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,13 @@ import {AuthService} from './services/auth.service';
 import { SingleAppareilComponent } from './single-appareil/single-appareil.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { AuthGuardService } from './services/auth.guard.service';
+import { EditAppareilComponent } from './edit-appareil/edit-appareil.component';
 
 
 const appRoutes: Routes = [
   {path: 'appareils', canActivate: [AuthGuardService], component: AppareilViewComponent},
   {path: 'appareils/:id', canActivate: [AuthGuardService], component : SingleAppareilComponent},
+  {path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent},
   {path: 'auth', component: AuthComponent},
   {path: '', component: AppareilViewComponent},
   {path: 'not-found', component : FourOhFourComponent},
@@ -32,7 +34,8 @@ const appRoutes: Routes = [
     AuthComponent,
     AppareilViewComponent,
     SingleAppareilComponent,
-    FourOhFourComponent
+    FourOhFourComponent,
+    EditAppareilComponent
   ],
   imports: [
     BrowserModule,
